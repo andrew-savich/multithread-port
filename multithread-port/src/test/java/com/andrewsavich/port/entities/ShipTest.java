538112e441c1ded975d684d4928a6626c2cd961e
andrew-savich <savichandrew@Gmail.com>
2021-04-26 08:13:14 +0300

    Added tests

---
+++ multithread-port/src/test/java/com/andrewsavich/port/entities/ShipTest.java
@@ -0,0 +1,34 @@
+package com.andrewsavich.port.entities;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ShipTest {
+
+	private static Ship underTest;
+
+	@BeforeAll
+	public static void init() {
+		underTest = new Ship("TestShip", 5);
+	}
+
+	@Test
+	public void unloadContainer_shouldThrowIllegalStateExceptionIfPierNull() {
+		assertThrows(IllegalStateException.class, () -> underTest.unloadContainer());
+	}
+
+	@Test
+	public void unloadContainer_shouldThrowIllegalStateExceptionIfContainersIsEmpty() {
+		assertThrows(IllegalStateException.class, () -> underTest.unloadContainer());
+	}
+
+	@Test
+	public void generateLoadedContainers_containersShouldBeNotEmpty() {
+		underTest.generateLoadedContainers();
+
+		assertTrue(underTest.getQuantityContainers() > 0);
+	}
+}
